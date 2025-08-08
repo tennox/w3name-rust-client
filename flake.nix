03ba24fbff8589efa55a191213b9e0ece239376c
Manu [tennox] <tennox+git@txlab.io>
2025-08-08 18:12:53 +0200

    Merge devenv-template/main [devenvious]
    squash-remote-commit: 2a66961bc31fed7e3be40bd59df5d60a12e07680 refs/remotes/devenv-template/main
    devenv-template-url: https://gitlab.com/txlab/dx/templates/rust.git

--- flake.nix
+++ flake.nix
@@ -1,35 +1,76 @@
 {
+  description = "w3name - A utility for managing IPNS names";
+
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default"; # (i) allows overriding systems easily
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+  outputs = inputs@{ self, nixpkgs, utils, naersk, systems, devenv, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = (import systems);
+      imports = [
+        inputs.devenv.flakeModule
+      ];
 
-        # native deps needed to build openssl and compile protobufs
-        native-deps = with pkgs; [ protobuf perl cmake ];
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage {
-          src = ./.;
+      # perSystem docs: https://flake.parts/module-arguments.html#persystem-module-parameters
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          naersk-lib = pkgs.callPackage naersk { };
+          
+          # native deps needed to build openssl and compile protobufs
+          native-deps = with pkgs; [ protobuf perl cmake ];
 
-          nativeBuildInputs = native-deps;
+          w3name-package = naersk-lib.buildPackage {
+            src = ./.;
+            nativeBuildInputs = native-deps;
+            cargoBuildOptions = opts: opts ++ ["--no-default-features"];
+          };
+        in
+        {
+          packages.default = w3name-package;
+          
+          apps.default = utils.lib.mkApp {
+            drv = w3name-package;
+          };
 
-          cargoBuildOptions = opts: opts ++ ["--no-default-features"];
+          devenv.shells.default = {
+            name = "w3name";
+            
+            imports = [
+              ./devenv.nix
+            ];
+            
+            # Useful packages for nix, so I put them here instead of devenv.nix
+            packages = with pkgs; [
+              nixpkgs-fmt
+              nil
+            ] ++ native-deps;
+          };
         };
 
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ] ++ native-deps;
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      });
+  nixConfig = {
+    extra-substituters = [
+      "https://devenv.cachix.org" # https://devenv.sh/binary-caching/
+      "https://nix-community.cachix.org" # for fenix
+    ];
+    extra-trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
