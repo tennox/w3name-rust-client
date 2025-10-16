a076afc5d4ca12659bef6a14119764ca40250095
Manu [tennox] <tennox+git@txlab.io>
2025-10-17 00:02:22 +0200

    flake.nix: add attic binary cache configuration

--- flake.nix
+++ flake.nix
@@ -99,10 +99,12 @@
     extra-substituters = [
       "https://devenv.cachix.org" # https://devenv.sh/binary-caching/
       "https://nix-community.cachix.org" # for fenix
+      "https://attic.tam.ma/public" # attic cache
     ];
     extra-trusted-public-keys = [
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "public:HotESZx/eJ2sQdxK6ZiMMcP13rm3vRl/IXbwTnapCdo="
     ];
   };
 }
